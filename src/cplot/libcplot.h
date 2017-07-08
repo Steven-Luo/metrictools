54b404bdfaf742d71bf8cc8c4bf39f2db7a71412
Paul Asmuth <paul@asmuth.com>
2017-07-08 17:55:43 +0200

    move core/libcplot -> src/cplot

---
+++ src/cplot/libcplot.h
@@ -0,0 +1,22 @@
+/**
+ * This file is part of the "libstx" project
+ *   Copyright (c) 2011-2014 Paul Asmuth, Google Inc.
+ *
+ * libstx is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include "cplot/axisdefinition.h"
+#include "cplot/areachart.h"
+#include "cplot/barchart.h"
+#include "cplot/canvas.h"
+#include "cplot/domain.h"
+#include "cplot/linechart.h"
+#include "cplot/pointchart.h"
+#include "cplot/series.h"
+#include "cplot/svgtarget.h"
+#include "cplot/timedomain.h"
+
+namespace cplot = stx::chart;
+
